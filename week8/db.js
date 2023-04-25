526fca5cb9c13c30ac6eec399a7fb6261292dcad
melkamu <melkemk503@gmail.com>
2023-04-25 20:41:15 +0300

    Start ignoring debug.log

--- week8/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const schema = mongoose.Schema({
-    email: { type: String, unique: true }, // Set unique property to true for email field
-    password: { type: String }
-});
-
-const model = mongoose.model('users', schema);
-export default model;
